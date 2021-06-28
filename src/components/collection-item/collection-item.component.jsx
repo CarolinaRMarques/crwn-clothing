f5c59aa6102c9324cbf9fd3f9ee62e185f745ebd
CarolinaRMarques <44268230+CarolinaRMarques@users.noreply.github.com>
2021-06-28 17:10:38 +0100

    organize imports

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,9 @@
+import CustomButton from "../custom-button/custom-button.component";
 import React from "react";
 import { connect } from "react-redux";
+
 import { addItem } from "../../redux/cart/cart.actions";
-import CustomButton from "../custom-button/custom-button.component";
+
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ item, addItem }) => {
