06efab874b9ea26b90790b6770ed4520391f42be
Carolina Marques <carolinaresendemarques@hotmail.com>
2021-07-06 14:44:22 +0100

    converted code using sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import { signUpStart } from "../../redux/user/user.actions";
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
 import "./sign-up.styles.scss";
@@ -7,6 +8,7 @@ import "./sign-up.styles.scss";
 class SignUp extends React.Component {
 	constructor() {
 		super();
+
 		this.state = {
 			displayName: "",
 			email: "",
@@ -14,34 +16,26 @@ class SignUp extends React.Component {
 			confirmPassword: "",
 		};
 	}
+
 	handleSubmit = async (event) => {
 		event.preventDefault();
+		const { signUpStart } = this.props;
 		const { displayName, email, password, confirmPassword } = this.state;
+
 		if (password !== confirmPassword) {
 			alert("Passwords don't match");
 			return;
 		}
-		try {
-			const { user } = await auth.createUserWithEmailAndPassword(
-				email,
-				password
-			);
-			this.setState({
-				displayName: "",
-				email: "",
-				password: "",
-				confirmPassword: "",
-			});
-			createUserProfileDocument(user, { displayName });
-		} catch (error) {
-			console.error(error);
-		}
+
+		signUpStart({ displayName, email, password });
 	};
 
 	handleChange = (event) => {
 		const { name, value } = event.target;
+
 		this.setState({ [name]: value });
 	};
+
 	render() {
 		const { displayName, email, password, confirmPassword } = this.state;
 		return (
@@ -89,4 +83,7 @@ class SignUp extends React.Component {
 	}
 }
 
-export default SignUp;
+const mapDispatchToProps = (dispatch) => ({
+	signUpStart: (userCrendentials) => dispatch(signUpStart(userCrendentials)),
+});
+export default connect(null, mapDispatchToProps)(SignUp);
--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,9 +1,9 @@
 import { all, call } from "redux-saga/effects";
 import { cartSagas } from "./cart/cart.saga";
 
-import { fetchCollectionsStart } from "./shop/shop.saga";
+import { shopSaga } from "./shop/shop.saga";
 import { userSagas } from "./user/user.saga";
 
 export default function* rootSaga() {
-	yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas)]);
+	yield all([call(shopSaga), call(userSagas), call(cartSagas)]);
 }
