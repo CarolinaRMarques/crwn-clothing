8b88adbfc1f734445140b22df48ddb246c64ecef
Carolina Marques <carolinaresendemarques@hotmail.com>
2021-07-06 17:59:21 +0100

    started using hooks in previous classes where componentDidMount existed

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -1,19 +1,18 @@
 import { connect } from "react-redux";
+import { compose } from "redux";
 import { createStructuredSelector } from "reselect";
 
-import { compose } from "redux";
-
+import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
-import { selectIsCollectionLoaded } from "../../redux/shop/shop.selectors";
 import CollectionPage from "./collection.component";
 
 const mapStateToProps = createStructuredSelector({
-	isLoading: (state) => !selectIsCollectionLoaded(state),
+	isLoading: (state) => !selectIsCollectionsLoaded(state),
 });
 
-const CollectionsPageContainer = compose(
+const CollectionPageContainer = compose(
 	connect(mapStateToProps),
 	WithSpinner
 )(CollectionPage);
 
-export default CollectionsPageContainer;
+export default CollectionPageContainer;
