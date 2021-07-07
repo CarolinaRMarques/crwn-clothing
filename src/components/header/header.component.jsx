49529fe6984f947fa52b2cf3a0b8f1eabe618b10
Carolina Marques <carolinaresendemarques@hotmail.com>
2021-07-07 11:12:37 +0100

    no contact in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { signOutStart } from "../../redux/user/user.actions";
@@ -10,8 +11,8 @@ import CartIcon from "../cart-icon/cart-icon.component";
 import {
 	HeaderContainer,
 	LogoContainer,
-	OptionsContainer,
 	OptionLink,
+	OptionsContainer,
 } from "./header.styles";
 
 const Header = ({ currentUser, hidden, signOutStart }) => (
